ccad547adf8e1e54527992e088282ba479d7f6e5
deborahjulietmoiforay <you@example.com>
2026-08-10 23:48:32 +0000

    Fix Vercel serverless function entry point with api/index.js and rewrites

---
+++ api/index.js
@@ -0,0 +1,9 @@
+'use strict';
+/**
+ * VERCEL SERVERLESS FUNCTION ENTRY POINT
+ * Exports Express application instance for Vercel Serverless Function execution.
+ */
+
+const app = require('../server');
+
+module.exports = app;
